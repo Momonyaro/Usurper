ad02022928a3fb41ade625112b632c93175ab84a
Sebastian Alkstrand <sebastian.alkstrand@gmail.com>
2020-06-17 16:26:37 +0200

    hmmm

---
+++ Usurper/Assets/Scripts/Map/MapLighter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+//Here we recive the viewport tileData and apply the correct ligthing to it based on time of day and lightSources
+public class MapLighter
+{
+    public Atlas.TileObject[,] LightPass(Atlas.TileObject[,] tileData, int radius)
+    {
+
+        //here we process line of sight for lightsources
+
+        return tileData;
+    }
+}
